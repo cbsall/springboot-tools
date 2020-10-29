02fd7d746b2b4cc6437af479483c437884513a0a
caibaoshan <caibaoshan@163.com>
2020-10-29 15:27:52 +0800

    Mybatis-plus  代码生成。

---
+++ mybatiso-plus-generator/src/test/java/springboot/tools/mybatisoplusgenerator/MybatisPlusGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.tools.mybatisoplusgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybatisPlusGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
